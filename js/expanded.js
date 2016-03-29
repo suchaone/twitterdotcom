8b6d43dffca23de1bfe8f1432346cf2be494dc13
suchaone <ilya@Ilyas-MacBook-Pro.local>
2016-03-29 17:38:47 -0400

    linkify

--- js/expanded.js
+++ js/expanded.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var args = location.search.replace("?","").replace(/%20/g," ").split("___"); 
   $(".username").html(args[0]);
-  $(".tweet-body").html(decodeURI(args[1]));
+  $(".tweet-body").html(linkify(decodeURI(args[1])));
   $(".timestamp").html($(".timestamp").html() + " " + ((new Date()).getFullYear() + 1));
 
   var img = "https://twitter.com/" + args[0] + "/profile_image?size=bigger";
@@ -12,4 +12,22 @@ $(document).ready(function() {
     img = "images/swarthyvillain.jpg";
 
   $(".avi").attr("src", img);
+
+  function linkify(inputText) {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+    
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+    
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+    
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+    
+    return replacedText;
+  }
 });
--- js/index.js
+++ js/index.js
@@ -14,6 +14,8 @@ $(document).ready(function() {
       var mentionPattern = /\B@[a-z0-9_-]+/gi;
       var mentions = this.gsx$tweet.$t.match(mentionPattern);
 
+      this.gsx$tweet.$t = linkify(this.gsx$tweet.$t);
+
       $(mentions).each(function() {
         $tweet.addClass("mention-" + this.replace("@",""));
       });
@@ -159,5 +161,23 @@ $(document).ready(function() {
       
       $("#timeline").addClass("profile");
     }
+  }
+
+  function linkify(inputText) {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+    
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+    
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+    
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+    
+    return replacedText;
   }
 });
