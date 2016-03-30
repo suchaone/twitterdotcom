3d3a47a425f48ca2c25e6f0f53a7d61274723abc
suchaone <ilya@Ilyas-MacBook-Pro.local>
2016-03-29 23:40:46 -0400

    no more xss maybe?

--- js/expanded.js
+++ js/expanded.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-  var args = location.search.replace("?","").replace(/%20/g," "); 
-  debugger;
+  var args = removeTags(location.search.replace("?","").replace(/%20/g," ")); 
   var username = args.split("___")[0];
   $(".username").html(username);
   $(".tweet-body").html(linkify(decodeURI(args.substr(args.indexOf("___") + 3, args.length))));
@@ -32,4 +31,29 @@ $(document).ready(function() {
     
     return replacedText;
   }
+
+  var tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
+  
+  var tagOrComment = new RegExp(
+      '<(?:'
+      // Comment body.
+      + '!--(?:(?:-*[^->])*--+|-?)'
+      // Special "raw text" elements whose content should be elided.
+      + '|script\\b' + tagBody + '>[\\s\\S]*?</script\\s*'
+      + '|style\\b' + tagBody + '>[\\s\\S]*?</style\\s*'
+      // Regular name
+      + '|/?[a-z]'
+      + tagBody
+      + ')>',
+      'gi');
+
+  function removeTags(html) {
+    var oldHtml;
+    do {
+      oldHtml = html;
+      html = html.replace(tagOrComment, '');
+    } while (html !== oldHtml);
+    return html.replace(/</g, '&lt;');
+  }
+
 });
